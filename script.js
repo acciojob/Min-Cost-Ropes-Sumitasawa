d72dade9743f5390c4f9acd0f3dbfab97d61e710
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-04 13:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  if(arr.length===0)return 0;
-	let total=0;
-	while(arr.length>1){
-		arr.sort((a,b)=>a-b);
-	const first = arr.shift();
+function mincost(arr) {
+  if (!Array.isArray(arr) || arr.length === 0) return 0;
+
+  let totalCost = 0;
+
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+
+    const first = arr.shift();
     const second = arr.shift();
 
     const cost = first + second;
     totalCost += cost;
-		 arr.push(cost);
-	}
-	return total;
-	  }
+    arr.push(cost);
+  }
 
-module.exports=mincost;
+  return totalCost;
+}
